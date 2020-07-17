280b22c28e7383c8b09bd7394cccde1ef3d17117
Felipe Acevedo <felipe.a.montoya@gmail.com>
2020-07-16 22:16:03 -0500

    Add setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # compensator
 A device to help deal with addictive habits
+
+## What it does
+For every "productive" hour, you will receive 15 minutes of leisure
--- compensator/compensator.ino
+++ compensator/compensator.ino
@@ -1,37 +1,24 @@
-/*
-  Blink
+#include "LedControl.h"
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+const int START_GOOD_ACTIVITY_BUTTON_PIN = 9;
+const int START_BAD_ACTIVITY_BUTTON_PIN = 8;
+const float NUMBER_OF_LEDS = 64;
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+int DEFAULT_SCREEN_INTENSITY = 15;
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
+LedControl lc = LedControl(12, 10, 11, 1); // Pins: DIN,CLK,CS, # of Displays connected
 
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // LED matrix setup
+  lc.shutdown(0, false); // Wake up display
+  lc.setIntensity(0, DEFAULT_SCREEN_INTENSITY);
+  lc.clearDisplay(0);
+
+  // buttons setup
+  pinMode(START_GOOD_ACTIVITY_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(START_BAD_ACTIVITY_BUTTON_PIN, INPUT_PULLUP);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+
 }
